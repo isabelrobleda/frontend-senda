6611375e351c4169cf7a100b8dedc4f6e2acb8d4
Isabel Robleda <isabel.robleda@gmail.com>
2024-09-19 17:56:01 +0200

    changes in CTA Home CSS for mobile

--- src/components/CTAApply.jsx
+++ src/components/CTAApply.jsx
@@ -13,16 +13,16 @@ function CTAApply() {
           alt="background green cta"
           className="relative z-0 md:rounded-none rounded-lg"
         />
-        <h3 className="absolute z-10 px-[40px] py-[56px] font-semibold font-pangea md:text-2xl text-lg">
+        <h3 className="absolute z-10 px-[40px] py-[35px] font-semibold font-pangea md:text-2xl text-lg">
           Promoviendo una cultura de <br /> pensamiento
         </h3>
-        <p className="absolute z-10 md:mt-32 mt-28 px-[40px] font-light md:w-1/2 text-sm md:text-base">
+        <p className="absolute z-10 md:mt-32 mt-24 px-[40px] font-light md:w-1/2 text-sm md:text-base">
           Fomentamos dignidad, ética y creatividad. Alumnos responsables,
           saludables y conscientes del medio ambiente, con habilidades digitales
           del siglo XXI.
         </p>
         <Link to={"/aplica-al-senda"}>
-          <div className="absolute  h-10 ml-10 px-8 z-10 md:bottom-18 bottom-10  py-2 bg-white hover:opacity-85 rounded-2xl border border-white ">
+          <div className="absolute  h-10 ml-10 px-8 z-10 md:bottom-18 bottom-7 py-2 bg-white hover:opacity-85 rounded-2xl border border-white ">
             <span className="text-black/90 md:text-base text-sm font-medium font-['Roboto'] leading-normal tracking-tight hover:cursor-pointer">
               Aplica ahora
             </span>
--- src/pages/Nosotros/Inspiracion.jsx
+++ src/pages/Nosotros/Inspiracion.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import InspiracionImg from "../../assets/inspiracion/inspiracion.png";
+import Conviccion from "../../assets/inspiracion/conviccion.png";
 
 function Inspiracion() {
   return (
     <div className="w-full flex flex-col items-center ">
       <div className="w-full h-auto  max-w-[1440px] bg-white flex flex-col md:flex-row justify-center items-start gap-12 pt-32 ">
         <div className="w-full h-auto flex left-0">
-          <img src="src/assets/inspiracion/inspiracion.png" alt="image inspiración" className="md:h-[500px] h-80 md:w-auto md:left-0 md:mx-[128px] ml-10" />
+          <img src={InspiracionImg} alt="image inspiración" className="md:h-[500px] h-80 md:w-auto md:left-0 md:mx-[128px] ml-10" />
         </div>
         <div className="w-full flex flex-col self-center items-start gap-4 p-8 md:mr-40">
           <div className="text-[#1e1e1e] text-2xl font-bold font-['Inter'] leading-tight">
@@ -61,7 +63,7 @@ function Inspiracion() {
           </div>
         </div>
         <div className="w-full md:w-1/2 flex justify-center md:justify-start">
-          <img src="src/assets/inspiracion/conviccion.png" alt="imagen convicción" className="md:h-[500px] h-80 md:w-auto md:left-0  mr-10" />
+          <img src={Conviccion} alt="imagen convicción" className="md:h-[500px] h-80 md:w-auto md:left-0  mr-10" />
         </div>
       </div>
     </div>
