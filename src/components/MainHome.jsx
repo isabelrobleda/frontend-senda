93c3d943054eaf3701746594d1b5f266f7f82274
Isabel Robleda <isabel.robleda@gmail.com>
2024-10-12 19:49:17 +0200

    css changes in main image home

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -8,10 +8,10 @@ import StarEmoji from "../assets/star-emoji.png";
 
 function MainHome() {
   return (
-    <div className="relative w-full md:h-[900px] px-4 md:px-16 py-20 md:py-40 flex-col justify-center items-center gap-8 inline-flex">
+    <div className="relative w-full md:h-screen px-4 md:px-16 py-20 md:py-40 flex-col justify-center items-center gap-8 inline-flex">
       <div className="absolute inset-0 z-0">
         <img
-          className="w-screen object-cover opacity-10 z-0 md:h-[900px]"
+          className="w-screen object-cover opacity-10 z-0 md:h-screen"
           src={HeaderBackground}
           alt="Background"
         />
