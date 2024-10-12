f13f3e994a4449905adbc88f83786b9557c5d910
Isabel Robleda <isabel.robleda@gmail.com>
2024-10-12 20:02:09 +0200

    cta changes in home

--- src/components/CTAApply.jsx
+++ src/components/CTAApply.jsx
@@ -5,7 +5,7 @@ import ImgMaternal from "../assets/CTAs/img-maternal.png";
 
 function CTAApply() {
   return (
-    <div className="relative md:h-[650px] w-full px-8 md:px-16 py-16 md:py-24 bg-white flex flex-col justify-center items-center gap-12">
+    <div className="relative md:h-[650px] w-full px-8 md:px-16 py-16 md:py-24 bg-[#f9f9fe] flex flex-col justify-center items-center gap-12">
       <div className="relative flex flex-col-reverse md:flex-row justify-start items-start">
         <div className="abolute flex flex-col justify-between">
         <img
@@ -13,8 +13,8 @@ function CTAApply() {
           alt="background green cta"
           className="relative z-0 md:rounded-none rounded-lg"
         />
-        <h3 className="absolute z-10 px-[40px] py-[35px] font-semibold font-pangea md:text-2xl text-base">
-          Promoviendo una cultura de <br /> pensamiento
+        <h3 className="absolute z-10 px-[40px] py-[35px] font-medium font-pangea md:text-[32px] text-base leading-9">
+          Promoviendo una cultura <br /> de pensamiento
         </h3>
         <p className="absolute z-10 md:mt-32 mt-24 px-[40px] font-light md:w-1/2 text-xs md:text-base">
           Fomentamos dignidad, ética y creatividad. Alumnos responsables,
