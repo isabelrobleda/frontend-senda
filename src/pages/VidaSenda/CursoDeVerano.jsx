8d835ba719466a41534939e7d253042614872e60
Isabel Robleda <isabel.robleda@gmail.com>
2026-05-13 16:08:23 +0200

    padding addition

--- src/pages/VidaSenda/CursoDeVerano.jsx
+++ src/pages/VidaSenda/CursoDeVerano.jsx
@@ -7,7 +7,7 @@ function CursoDeVerano() {
   return (
     <div>
       {/* Main Image */}
-      <div className="w-full flex justify-center items-center px-8 md:px-[200px] pt-5">
+      <div className="w-full flex justify-center items-center px-8 md:px-[200px] pt-5 md:py-[200px]">
         <img
           src={CursoDeVeranoImg}
           alt="Curso de Verano"
